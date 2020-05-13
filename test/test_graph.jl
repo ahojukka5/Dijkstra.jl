154575f98bc0b1e4369ebefd2858a71b28c19d24
Jukka Aho <ahojukka5@gmail.com>
2020-05-13 16:26:48 +0700

    Add test for generate_random_graph

--- test/test_graph.jl
+++ test/test_graph.jl
@@ -16,3 +16,14 @@ add_edge!(G, 1, 3, 2)
 # Graph also stores adjacency lists, and adjacent nodes can be returned using
 # `get_adjacent_nodes`:
 @test get_adjacent_nodes(G, 1) == Set([2, 3])
+
+# Random graphs can be generated using `generate_random_graph`. For example,
+# to generate graph with 10 nodes, edge density 0.5 and smallest weight 1 and
+# largest weight 10:
+
+using Random
+Random.seed!(1)
+G = generate_random_graph(10, 0.5, 1, 10)
+adj1 = get_adjacent_nodes(G, 0)
+adj2 = Set([1, 2, 6, 7, 9])
+@test setdiff(adj1, adj2) == Set([])
